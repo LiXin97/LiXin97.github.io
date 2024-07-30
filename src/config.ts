ebc7616ad1bcddc5404d0a90abc8d97beaba7c73
XinLi <lixin.1997.lixin@gmail.com>
2024-07-30 10:18:14 +0800

    update domain redirect error

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "http://blog.lixin.ai/", // replace this with your deployed domain
+  website: "http://lixin.ai/", // replace this with your deployed domain
   author: "Xin Li",
-  profile: "http://blog.lixin.ai/",
+  profile: "http://lixin.ai/",
   desc: "Xin Li's Homepage, Research Assistant at Nanyang Technological University (NTU), Singapore. Research Interest: SLAM, Embodied AI, and Scene Understanding.",
   title: "Xin Li",
   ogImage: "assets/XinLi.jpg",
